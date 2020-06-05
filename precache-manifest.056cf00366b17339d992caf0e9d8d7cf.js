e5dfd783a276b0045281f450f1da116d76d00e1d
lngwa <lewische91@gmail.com>
2020-06-04 19:43:46 -0500

    Updates

---
+++ precache-manifest.056cf00366b17339d992caf0e9d8d7cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23f32930d6471fe00e2b47ff753fb22",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b7182fcf4e12170f9d7f",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/static/css/main.b199687b.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7182fcf4e12170f9d7f",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/static/js/main.82af5808.chunk.js"
+  },
+  {
+    "revision": "31a0a5e5dda550bf36fc",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/static/js/runtime-main.fb7d1890.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/precache-manifest.056cf00366b17339d992caf0e9d8d7cf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lngwa/Web-Developer-Master-Classs/tree/master/robofriends/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
